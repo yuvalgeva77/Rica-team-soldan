bee28093aa5fa38645d8ce78e72a62793a987802
Eran <eran197@gmail.com>
2020-12-31 16:30:39 +0200

    Update Soldan Scanner.py

--- Soldan Scanner.py
+++ Soldan Scanner.py
@@ -1,9 +1,10 @@
+import subprocess
+import webbrowser as wb
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
-import subprocess
 from kivy.uix.popup import Popup
 
 
@@ -22,7 +23,6 @@ class SoldanScanner(App):
 
     def do_quit_button(self, obj):
         quit()
-
     def report(self, instance):
         #print("hi")
         popup = Popup(title='Test popup',
@@ -33,6 +33,12 @@ class SoldanScanner(App):
         subprocess.call('net user | find "Administrator"', shell=True)
         #regex : (?<=Administrator).*(?<=^\S*\s+)\S+ , need to get rid of first whitespaces
 
+    def do_nessus_install_button(self, instance):
+        wb.open("https://de.tenable.com/products/nessus")
+
+    def do_nessus_button(self, instance):
+        wb.open("https://localhost:8834/")
+
     def build(self):
         layout = GridLayout(cols=2, row_force_default=True, row_default_height=100)
 
@@ -40,12 +46,15 @@ class SoldanScanner(App):
         report_button.bind(on_press=self.report)
         email_button = Button(text="Check email", pos=(100, 350), size_hint=(.10, .10))
         kismet_button = Button(text="Start Kismet", pos=(100, 350), size_hint=(.10, .10))
+        nessus_install_button = Button(text="Install Nessus", pos=(100, 350), size_hint=(.10, .10))
+        nessus_install_button.bind(on_press = self.do_nessus_install_button)
         nessus_button = Button(text="Start Nessus", pos=(100, 350), size_hint=(.10, .10))
+        nessus_button.bind(on_press = self.do_nessus_button)
         av_button = Button(text="Start Antivirus", pos=(100, 350), size_hint=(.10, .10))
         quit_button = Button(text="Quit", pos=(100, 350), size_hint=(.10, .10))
         quit_button.bind(on_press=self.do_quit_button)
 
-        for but in [report_button, email_button, kismet_button, nessus_button, av_button, quit_button]:
+        for but in [report_button, email_button, kismet_button, nessus_button, nessus_install_button, av_button, quit_button]:
             layout.add_widget(but)
 
         return layout
