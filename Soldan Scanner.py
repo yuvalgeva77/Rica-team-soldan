4061142d031c484bf1a82749d23767a9505d17f7
CarinaEl <43412323+CarinaEl@users.noreply.github.com>
2021-01-09 17:59:01 +0100

    report
    fixed report button

--- Soldan Scanner.py
+++ Soldan Scanner.py
@@ -7,17 +7,9 @@ from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
 
-
-##############example of a console command run via python. this reads out the model of a USB stick.
-######### just change the string in the line cmd= .. or in the Popen strings.
-# def model_USB(self, blockdev):
-#     cmd = "udevadm info --query=all --name=/dev/{0} ".format(blockdev)  # want to find the model number
-#     p1 = Popen(shlex.split(cmd), stdout=PIPE)
-#     p2 = Popen(["grep", "ID_MODEL"], stdin=p1.stdout, stdout=PIPE, stderr=PIPE)
-#     p3 = Popen(["grep", "-v", "ID_MODEL_ENC"], stdin=p2.stdout, stdout=PIPE, stderr=PIPE)
-#     output, err = p3.communicate()
-#     return output.decode('ascii')[12:-1]
 
 
 class SoldanScanner(App):
@@ -25,14 +17,13 @@ class SoldanScanner(App):
     def do_quit_button(self, obj):
         quit()
     def report(self, instance):
-        #print("hi")
-        popup = Popup(title='Test popup',
-            content = Label(text='Hello world'),
-            size_hint = (None, None), size = (400, 400),
+        with open('report.txt') as file:
+            content= file.read()
+        popup = Popup(title='Report',
+            content = Label(text=content),
+            size_hint = (None, None), size = (700, 700),
             auto_dismiss = True)
         popup.open()
-        subprocess.call('net user | find "Administrator"', shell=True)
-        #regex : (?<=Administrator).*(?<=^\S*\s+)\S+ , need to get rid of first whitespaces
 
     def do_nessus_install_button(self, instance):
         wb.open("https://de.tenable.com/products/nessus")
@@ -49,8 +40,6 @@ class SoldanScanner(App):
 
         report_button = Button(text="Show report", pos=(300, 350), size_hint=(.10, .10))
         report_button.bind(on_press=self.report)
-        email_button = Button(text="Check email", pos=(100, 350), size_hint=(.10, .10))
-        kismet_button = Button(text="Start Kismet", pos=(100, 350), size_hint=(.10, .10))
         nessus_install_button = Button(text="Install Nessus", pos=(100, 350), size_hint=(.10, .10))
         nessus_install_button.bind(on_press = self.do_nessus_install_button)
         nessus_button = Button(text="Start Nessus", pos=(100, 350), size_hint=(.10, .10))
@@ -60,7 +49,7 @@ class SoldanScanner(App):
         quit_button = Button(text="Quit", pos=(100, 350), size_hint=(.10, .10))
         quit_button.bind(on_press=self.do_quit_button)
 
-        for but in [report_button, email_button, kismet_button, nessus_button, nessus_install_button, av_button, quit_button]:
+        for but in [report_button, nessus_button, nessus_install_button, av_button, quit_button]:
             layout.add_widget(but)
 
         return layout
